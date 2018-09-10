543d81d67e1d4885cf0768da823faf9c3550e4d3
Meghana Bhimasani <m.bhimasani@gmail.com>
2018-09-10 14:26:10 -0400

    final

--- Python-Challenge/PyBank/main.py
+++ Python-Challenge/PyBank/main.py
@@ -12,18 +12,18 @@ with open(budget_csv, newline="") as csvfile:
     # Read and skip the header row first
     csv_header = next(csvfile)
 
-    # counting index and row
+    net_amount = 867884
     for counter, row in enumerate(csvreader):
         if counter > 0:
             # read only the first row after header
             break
+
         first_value = row[1] # store profit/loss of Jan-2010
         prev_profitloss = row[1] # setting the profit/loss amount in the Jan-2010 as 'previous profit/loss' ie. what the profit/loss amount in Feb-2010 is subtracting to find change between Jan-2010 and Feb-2010
-
+    net_amount += int(row[1])
     total_months = 2 #have to start at 2 since we already read the first 2 rows including header
-    net_amount = 0
-    for row in csvreader:
 
+    for row in csvreader:
         # Find totals
         total_months += 1
         net_amount += int(row[1])
@@ -42,8 +42,10 @@ with open(budget_csv, newline="") as csvfile:
             minimum[0] = row[0]
             minimum[1] = monthly_change
 
+
     last_value = row[1] #store profit/loss of Feb-2017
     average = round(((int(last_value) - int(first_value))/(int(total_months - 1))), 2) #calculating average change between months over entire period, rounded to 2 decimals
+
 
 output = (
     "\nFinancial Analysis\n"
